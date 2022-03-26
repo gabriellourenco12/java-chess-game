c8a4f9e3a9739525c21bcccab30a829b5f00e6e7
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-03-26 12:26:23 -0300

    implementation of checkmate logic

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -18,7 +19,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.isCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -27,6 +28,7 @@ public class Main {
                 ChessPosition source = UI.readChessPosition(sc);
 
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                Runtime.getRuntime().exec("clear");
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
@@ -42,7 +44,11 @@ public class Main {
             } catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
